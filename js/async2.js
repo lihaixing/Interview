895bad47edfeab9fd28b932aabf735973cccd338
李海兴 <lihaixing@xylink.com>
2021-03-07 08:03:15 +0800

    fix: no use

--- js/async2.js
+++ js/async2.js
@@ -1,1 +1,10 @@
+
+function sleep(duration) {
+    let start = Date.now();
+
+    while (duration + start > Date.now()) {
+
+    }
+}
+sleep(2000)
 console.log('async2');
--- js/defer2.js
+++ js/defer2.js
@@ -1,1 +1,10 @@
+
+function sleep(duration) {
+    let start = Date.now();
+
+    while (duration + start > Date.now()) {
+
+    }
+}
+sleep(0)
 console.log('defer2');
